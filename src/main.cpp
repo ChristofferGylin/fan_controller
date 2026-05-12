5730aecf7e4e7c11b5d8b48d96eaca94316d7e57
Christoffer Gylin <christoffer@christoffergylin.com>
2026-05-13 01:11:39 +0200

    Created basic PWM fan controller

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+const int FAN_PWM_PIN = 9;
+
+void setup() {
+    pinMode(FAN_PWM_PIN, OUTPUT);
+
+    TCCR1A = 0;
+    TCCR1B = 0;
+
+    TCCR1A |= (1 << COM1A1) | (1 << WGM11);
+    TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS10);
+
+    ICR1 = 320;
+
+    OCR1A = 160;
+}
+
+void loop() {
+}
